190533a8fe002cb0f44876cf76a6c0dc2050d45b
Erick Mendes <dev.erickmendes126@gmail.com>
2025-07-28 20:09:58 -0300

    Inserção de usuário com POST

--- src/main/java/com/erickmendes/workshop_mongodb/resources/UserResource.java
+++ src/main/java/com/erickmendes/workshop_mongodb/resources/UserResource.java
@@ -6,7 +6,9 @@ import com.erickmendes.workshop_mongodb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,5 +33,13 @@ public class UserResource {
         User user = service.findById(id);
         UserDTO userDTO = new UserDTO(user);
         return ResponseEntity.ok().body(userDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
+        User user = service.fromDTO(objDTO);
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/erickmendes/workshop_mongodb/service/UserService.java
+++ src/main/java/com/erickmendes/workshop_mongodb/service/UserService.java
@@ -1,6 +1,7 @@
 package com.erickmendes.workshop_mongodb.service;
 
 import com.erickmendes.workshop_mongodb.domain.User;
+import com.erickmendes.workshop_mongodb.dto.UserDTO;
 import com.erickmendes.workshop_mongodb.repository.UserRepository;
 import com.erickmendes.workshop_mongodb.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
