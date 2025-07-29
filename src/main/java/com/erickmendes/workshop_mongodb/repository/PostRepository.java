3429dbc901d005bb9da27882c6bcb7edb4a334af
Erick Mendes <dev.erickmendes126@gmail.com>
2025-07-29 13:18:35 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/erickmendes/workshop_mongodb/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.erickmendes.workshop_mongodb.repository;
+
+import com.erickmendes.workshop_mongodb.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
