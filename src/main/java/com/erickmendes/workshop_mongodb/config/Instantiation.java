ac7f9980f08659c9ab9bdce1d90814944f7bf548
Erick Mendes <dev.erickmendes126@gmail.com>
2025-07-30 12:16:18 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/erickmendes/workshop_mongodb/config/Instantiation.java
+++ src/main/java/com/erickmendes/workshop_mongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.erickmendes.workshop_mongodb.config;
 
 import com.erickmendes.workshop_mongodb.domain.Post;
 import com.erickmendes.workshop_mongodb.domain.User;
+import com.erickmendes.workshop_mongodb.dto.AuthorDTO;
 import com.erickmendes.workshop_mongodb.repository.PostRepository;
 import com.erickmendes.workshop_mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,8 +30,8 @@ public class Instantiation implements CommandLineRunner {
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post p1 = new Post(null, Instant.parse("2025-03-21T13:08:00Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post p2 = new Post(null, Instant.parse("2025-03-22T08:30:00Z"), "Bom dia", "Acordei feliz hoje!", maria);
+        Post p1 = new Post(null, Instant.parse("2025-03-21T13:08:00Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post p2 = new Post(null, Instant.parse("2025-03-22T08:30:00Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(p1, p2));
     }
 }
