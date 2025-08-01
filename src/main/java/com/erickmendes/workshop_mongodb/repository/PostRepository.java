47326b47e64f88f15e717fce81a85603f83fa146
Erick Mendes <dev.erickmendes126@gmail.com>
2025-08-01 09:43:25 -0300

    Consulta simples com @Query

--- src/main/java/com/erickmendes/workshop_mongodb/repository/PostRepository.java
+++ src/main/java/com/erickmendes/workshop_mongodb/repository/PostRepository.java
@@ -2,11 +2,16 @@ package com.erickmendes.workshop_mongodb.repository;
 
 import com.erickmendes.workshop_mongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
